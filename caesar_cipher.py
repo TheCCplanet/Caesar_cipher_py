375215ac661b7f9fdc7a2384b584f285a565f40c
TheCCplanet <129336515+TheCCplanet@users.noreply.github.com>
2023-03-30 02:07:54 +0330

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,45 @@
+plain_text = input("What is your plaintext? ")
+shift = int(input("What is your shift?   between 0 - 25 :"))
+cipher_code = list()
+this_one = list()
+
+def outside(num,fst,last):
+    while True :
+        if num > last :
+            return "trt again"
+        elif num < fst :
+            break
+
+
+def caesar(plain_text , shift):
+    #plain_text = (plain_text).lower()
+    cipher_range = int()
+
+    for letter in plain_text :
+        cipher_range = ord(letter) + shift
+        if letter != ' '  and cipher_range <= 122:
+            cipher_code.append(chr((ord(letter))+shift))
+        elif letter == " " :
+            cipher_code.append(chr(ord(letter)))
+        elif cipher_range > 122 :
+            cipher_code.append((chr((ord(letter))+shift-26)))
+    return cipher_code
+print (caesar(plain_text , shift))
+
+def encode_caesar(text_code):
+    cipher_range = int()
+    for letter in text_code :
+        difference = ord(letter) - ord("h")
+        if difference < 0  and cipher_range <= 122 :
+            cipher_code.append(chr((ord(letter))-(abs(difference))))
+    return (cipher_code)
+
+#def caesar(plainText, shift):
+#     for letter in plainText.lower() :
+#         data.append(chr((ord(letter))+shift))
+#     return data
+# print(caesar(plainText, shift))
+# for letter in data :
+#     this_one.append(chr((ord(letter))-shift))
+# print('encode', this_one)
+
